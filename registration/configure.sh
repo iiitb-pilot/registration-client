8b0f2f167bfbf39da946f8a296bb10864a3ede4c
Malleshdappu <93384116+Malleshdappu@users.noreply.github.com>
2024-11-23 09:43:46 +0530

    removed timestamp url env

--- registration/configure.sh
+++ registration/configure.sh
@@ -111,8 +111,8 @@ echo ")" >> "${work_dir}"/registration-client/target/run.bat
 cp "${work_dir}"/registration-client/target/run.bat "${work_dir}"/registration-client/target/lib/114to1201_run.bat
 
 ## jar signing
-jarsigner -keystore "${work_dir}"/build_files/keystore.p12 -storepass ${keystore_secret} -tsa ${signer_timestamp_url_env} -digestalg SHA-256 "${work_dir}"/registration-client/target/lib/registration-client-${client_version_env}.jar CodeSigning
-jarsigner -keystore "${work_dir}"/build_files/keystore.p12 -storepass ${keystore_secret} -tsa ${signer_timestamp_url_env} -digestalg SHA-256 "${work_dir}"/registration-client/target/lib/registration-services-${client_version_env}.jar CodeSigning
+jarsigner -keystore "${work_dir}"/build_files/keystore.p12 -storepass ${keystore_secret} -digestalg SHA-256 "${work_dir}"/registration-client/target/lib/registration-client-${client_version_env}.jar CodeSigning
+jarsigner -keystore "${work_dir}"/build_files/keystore.p12 -storepass ${keystore_secret} -digestalg SHA-256 "${work_dir}"/registration-client/target/lib/registration-services-${client_version_env}.jar CodeSigning
 
 /usr/local/openjdk-11/bin/java -cp "${work_dir}"/registration-client/target/registration-client-${client_version_env}.jar:"${work_dir}"/registration-client/target/lib/* io.mosip.registration.update.ManifestCreator "${client_version_env}" "${work_dir}/registration-client/target/lib" "${work_dir}/registration-client/target"
 
